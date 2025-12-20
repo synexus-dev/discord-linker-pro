ed1ba9700342308cdc20263e0fdcd68d7a1d8b92
Synexus Dev <synexus.dev@protonmail.com>
2025-12-20 00:14:52 -0300

    update model

--- src/main/java/dev/synexus/discordlinkerpro/domain/model/SyncRule.java
+++ src/main/java/dev/synexus/discordlinkerpro/domain/model/SyncRule.java
@@ -4,9 +4,9 @@ import lombok.Data;
 
 @Data
 public class SyncRule {
-    private final String permissionNode;  // ex: "synexus.vip.master"
-    private final String discordRoleId;   // ex: "987654321098765432"
-    private final SyncType type;          // Enum: ADD_ONLY, REMOVE_ONLY, SYNC
+    private final String permissionNode;
+    private final String discordRoleId;
+    private final SyncType type;
 
     public SyncRule(String permissionNode, String discordRoleId, SyncType type) {
         this.permissionNode = permissionNode;
